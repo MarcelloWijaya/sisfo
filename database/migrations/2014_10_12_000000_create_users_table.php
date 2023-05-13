d85be79ef85b719e34c53c7fb213f9075ee237b4
Marcello William Wijaya <marcellowilliam74@gmail.com>
2023-05-13 14:14:09 +0700

    user dan user role

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->foreignId('role_id');
+            $table->string('username');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->foreignId('is_active');
             $table->rememberToken();
             $table->timestamps();
         });
